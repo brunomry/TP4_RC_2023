fbd8327ae6116b808193e403e111530cf9d923ec
brunomry <brunomadozzo@gmail.com>
2023-11-24 07:26:02 -0300

    se agregó un mensaje faltante en ejercicio N°5 - TP4

--- JS/ejercicio5-tp4.js
+++ JS/ejercicio5-tp4.js
@@ -25,27 +25,27 @@ class Persona{
 
   esMayorDeEdad(){
     if(this.edad >= 18){
-      elementoSection += `<h4 class="mt-3">${this.nombre} es Mayor de Edad</h4>`;
+      elementoSection += `<p class="mt-3">${this.nombre} es <span class="fw-bold">Mayor de Edad</span></p>`;
     }else{
-      elementoSection += `<h4 class="mt-3">${this.nombre} es Menor de Edad</h4>`;
+      elementoSection += `<p class="mt-3">${this.nombre} es <span class="fw-bold">Menor de Edad</span></p>`;
     }
   }
 
   mostrarGeneracion(){
     if(this.añoNacimiento >= 1930 && this.añoNacimiento <= 1948){
-      elementoSection += `<h4 class="mt-3">${this.nombre} es Generación Silent Generation</h4>`;
+      elementoSection += `<p class="mt-3">${this.nombre} es <span class="fw-bold">Generación Silent Generation</span> y su Rasgo característico es <span class="fw-bold">Austeridad</span></p>`;
     }
     if(this.añoNacimiento >= 1949 && this.añoNacimiento <= 1968){
-      elementoSection += `<h4 class="mt-3">${this.nombre} es Generación Baby Boom</h4>`;
+      elementoSection += `<p class="mt-3">${this.nombre} es <span class="fw-bold">Generación Baby Boom</span> y su Rasgo característico es <span class="fw-bold">Ambición</span></p>`;
     }
     if(this.añoNacimiento >= 1969 && this.añoNacimiento <= 1980){
-      elementoSection += `<h4 class="mt-3">${this.nombre} es Generación X</h4>`;
+      elementoSection += `<p class="mt-3">${this.nombre} es <span class="fw-bold">Generación X</span> y su Rasgo característico es <span class="fw-bold">Obsesión por el éxito</span></p>`;
     }
     if(this.añoNacimiento >= 1981 && this.añoNacimiento <= 1993){
-      elementoSection += `<h4 class="mt-3">${this.nombre} es Generación Y</h4>`;
+      elementoSection += `<p class="mt-3">${this.nombre} es <span class="fw-bold">Generación Y</span> y su Rasgo característico es <span class="fw-bold">Frustración</span></p>`;
     }
     if(this.añoNacimiento >= 1994 && this.añoNacimiento <= 2010){
-      elementoSection += `<h4 class="mt-3">${this.nombre} es Generación Z</h4>`;
+      elementoSection += `<p class="mt-3">${this.nombre} es <span class="fw-bold">Generación Z</span> y su Rasgo característico es <span class="fw-bold">Irreverencia</span></p>`;
     }
   }
 
